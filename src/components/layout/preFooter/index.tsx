2f7be6d815fe31e136acd1aed248d8f8e36f5a27
Emiliano Yaryura <emilianoyaryurat@gmail.com>
2021-09-14 18:27:12 -0300

    fix: prefooter language

--- src/components/layout/preFooter/index.tsx
+++ src/components/layout/preFooter/index.tsx
@@ -7,15 +7,15 @@ const PreFooter = () => {
   return (
     <Container size="large" className="flex flex-col items-center mt-80">
       <h1 className={clsx('uppercase text-center', s.title)}>
-        suscribe to our <br /> newsletter
+        suscribite a nuestra <br /> newsletter
       </h1>
       <div className="mt-8 sm:mt-14 flex flex-col sm:flex-row w-full sm:w-min">
         <input
           type="email"
           className="bg-gray-200 w-full text-center sm:text-left mb-3 sm:mb-0 py-4 mr-4 rounded-lg text-14 px-5 outline-none transition-all duration-200 border-2 border-solid border-gray-200 focus:border-blue focus:outline-none sm:w-96"
-          placeholder="Enter your email address for updates"
+          placeholder="Ingresa tu mail para más novedades"
         />
-        <Button type="primary">Suscribe</Button>
+        <Button type="primary">Suscribirse</Button>
       </div>
     </Container>
   )
