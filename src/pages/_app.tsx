b8b8dd2742e8a25cc4a5e0cacbc187780f404bf2
Emiliano Yaryura <emilianoyaryurat@gmail.com>
2021-09-20 12:09:53 -0300

    feat: testing ga

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,25 @@
 import { AppProps } from 'next/app'
 import 'css/global.css'
+import { useRouter } from 'next/dist/client/router'
+import { useEffect } from 'react'
 
-const App = ({ Component, pageProps }: AppProps) => <Component {...pageProps} />
+const isProduction = process.env.NODE_ENV === 'production'
+
+const App = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = (url: URL) => {
+      /* invoke analytics function only for production */
+      // @ts-ignore
+      if (isProduction) gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+  return <Component {...pageProps} />
+}
 
 export default App
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -6,6 +6,9 @@ import Document, {
   DocumentContext
 } from 'next/document'
 
+const GA_TRACKING_ID = process.env.GOOGLE_ANALYTICS_ID
+const isProduction = process.env.NODE_ENV === 'production'
+
 class MyDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx)
@@ -15,7 +18,30 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          {/* enable analytics script only for production */}
+          {isProduction && (
+            <>
+              <script
+                async
+                src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+              />
+              <script
+                // eslint-disable-next-line react/no-danger
+                dangerouslySetInnerHTML={{
+                  __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `
+                }}
+              />
+            </>
+          )}
+        </Head>
         <body>
           <Main />
           <NextScript />
