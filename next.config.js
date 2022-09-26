249c0de0097d04ab7e4b175d17e37c7667ccce9d
emilianoyaryura <emilianoyaryurat@gmail.com>
2022-09-25 21:28:36 -0300

    fix build

--- next.config.js
+++ next.config.js
@@ -10,6 +10,11 @@ module.exports = withPlugins(
     withSvgr({
       images: {
         domains: ['images.ctfassets.net', 'images.unsplash.com']
+      },
+      env: {
+        NEXT_PUBLIC_SUPABASE_PROJECT_URL:
+          process.env.NEXT_PUBLIC_SUPABASE_PROJECT_URL,
+        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
       }
     })
   ],
