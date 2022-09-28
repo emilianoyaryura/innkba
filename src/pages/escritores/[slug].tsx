68e9139251a7824a909489c3f1668c6354483523
emilianoyaryura <emilianoyaryurat@gmail.com>
2022-09-28 00:44:40 -0300

    authors

--- src/pages/escritores/[slug].tsx
+++ src/pages/escritores/[slug].tsx
@@ -1,10 +1,14 @@
 import AuthorSocial from 'components/atoms/author-social'
 import Container from 'components/layout/container'
 import PageLayout from 'components/layout/pageLayout'
+import PostGrid from 'components/molecules/postGrid'
 import { getAllAuthors, getPostsPreview, getSingleAuthor } from 'lib/api'
 import { getSectionSlug } from 'lib/utils/section'
 import Image from 'next/image'
 import { Author, AuthorPreview, PostPreview } from 'ts/models'
+import { supabase } from 'lib/supabase-client'
+import EyeIcon from 'components/atoms/icons/eye'
+import { useCallback, useEffect, useState } from 'react'
 
 const WriterPage = ({
   author,
@@ -15,6 +19,8 @@ const WriterPage = ({
   posts: PostPreview[]
   authors: AuthorPreview[]
 }) => {
+  const [views, setViews] = useState<null | number>(null)
+
   const tinyPosts = posts?.map((p) => {
     const section = getSectionSlug(p.category)
     return {
@@ -37,28 +43,68 @@ const WriterPage = ({
   // @ts-ignore
   const searcher = tinyPosts?.concat(authorsSearcher)
 
+  const authorPosts = posts.filter((e) => e.author.name === author.name)
+
+  const authorPostsSlug = authorPosts.map((e) => e.slug)
+
+  const handleViews = useCallback(async () => {
+    try {
+      const { data } = await supabase
+        .from('Page Views')
+        .select('slug, views')
+        .filter('slug', 'in', `(${authorPostsSlug})`)
+      try {
+        // @ts-ignore
+        const sum = data
+          ?.map((e) => e.views)
+          .reduce((partialSum, a) => partialSum + a, 0)
+        setViews(sum)
+        //   const views = data[0]?.views
+        //   const a = await supabase
+        //     .from('Page Views')
+        //     .update({ slug: query, views: views + 1 })
+        //   const b = setViews(views + 1)
+        //   return a && b
+      } catch (err) {
+        console.log(err, 'error')
+      }
+    } catch (err) {
+      console.log(err, 'error')
+    }
+  }, [authorPostsSlug])
+
+  useEffect(() => {
+    handleViews()
+  }, [handleViews])
+
   return (
     <PageLayout
       posts={searcher}
       headProps={{ title: `Innk ba | ${author?.name}` }}
     >
-      <Container size="small" className="flex flex-col items-center">
-        <div
-          className="w-full h-64 rounded-2xl"
-          style={{
-            backgroundColor:
-              author?.background === 'violet'
-                ? '#6C63FF'
-                : author?.background === 'blue'
-                ? '#2E8AF6'
-                : author?.background === 'yellow'
-                ? '#F9A826'
-                : author?.background === 'red'
-                ? '#FF4747'
-                : '#CECECE'
-          }}
-        />
-        <div className="rounded-full p-1 w-32 h-32 -mt-16 bg-white">
+      <div className="max-w-4xl mx-auto flex flex-col items-center mb-14">
+        <div className="w-full flex flex-col">
+          <div
+            className="w-full h-40 sm:h-48 md:h-64 lg:rounded-2xl"
+            style={{
+              backgroundColor:
+                author?.background === 'violet'
+                  ? '#6C63FF'
+                  : author?.background === 'blue'
+                  ? '#2E8AF6'
+                  : author?.background === 'yellow'
+                  ? '#F9A826'
+                  : author?.background === 'red'
+                  ? '#FF4747'
+                  : '#CECECE'
+            }}
+          />
+          <div className="flex items-center self-end mt-2 mr-2 text-gray-800">
+            <EyeIcon />
+            <p>{views}</p>
+          </div>
+        </div>
+        <div className="rounded-full p-1 w-32 h-32 -mt-24 bg-white">
           {author?.image ? (
             <Image
               alt={author?.name}
@@ -74,11 +120,16 @@ const WriterPage = ({
             </div>
           )}
         </div>
-        <p className="text-22 font-medium mt-2">{author?.name}</p>
-        <p className="text-16 text-center mt-2 text-gray-700 max-w-md">
-          {author.shortDescription}
-        </p>
-        <AuthorSocial author={author} className="mx-auto mt-6" />
+        <div className="mt-2 px-4 flex flex-col items-center">
+          <p className="text-22 font-medium">{author?.name}</p>
+          <p className="text-16 text-center mt-2 text-gray-700 max-w-md">
+            {author.shortDescription}
+          </p>
+          <AuthorSocial author={author} className="mx-auto mt-6" />
+        </div>
+      </div>
+      <Container size="large">
+        <PostGrid id="" posts={authorPosts} />
       </Container>
     </PageLayout>
   )
