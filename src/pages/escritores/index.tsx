e478c8de47cc8dc56592bd80a740b2aeb34d907a
emilianoyaryura <emilianoyaryurat@gmail.com>
2022-09-28 01:27:28 -0300

    fix build

--- src/pages/escritores/index.tsx
+++ src/pages/escritores/index.tsx
@@ -1,3 +1,18 @@
 import HomePage from 'pages'
+import { getAllAuthors, getHomePage, getPostsPreview } from 'lib/api'
+
+export const getStaticProps = async () => {
+  const posts = await getPostsPreview()
+  const page = await getHomePage()
+  const authors = await getAllAuthors()
+
+  return {
+    props: {
+      contentfulPosts: posts ?? null,
+      page: page ?? null,
+      authors: authors ?? null
+    }
+  }
+}
 
 export default HomePage
