6d65aa87df720b0b5dee1a2e908e092e91fca542
emilianoyaryura <emilianoyaryurat@gmail.com>
2022-10-05 17:21:54 -0300

    remove weekly quote label

--- src/components/atoms/quote/index.tsx
+++ src/components/atoms/quote/index.tsx
@@ -10,9 +10,6 @@ const Quote = ({ quote, author, className }: QuoteProps) => {
         className="my-36 bg-offBlue border border-solid border-gray-100 flex flex-col md:flex-row items-center md:items-start space-y-6 md:space-y-0 md:space-x-12 box-border px-8 py-8 sm:py-12 md:py-16 sm:px-16 rounded-2xl"
       >
         <div className="max-w-md md:max-w-5xl mx-auto mt-4 flex flex-col items-center md:items-start">
-          <h2 className="font-extrabold text-14 uppercase">
-            Quote de la semana
-          </h2>
           <h1
             className={clsx(
               'font-medium text-22 md:text-28 text-center md:text-left lg:text-38 md:leading-snug mt-2 md:mt-4 lg:mt-6'
