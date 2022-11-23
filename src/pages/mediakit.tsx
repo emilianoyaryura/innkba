66594be0b88ff7415d29e0cc11c6fceea07a8349
emilianoyaryura <emilianoyaryurat@gmail.com>
2022-11-23 11:05:49 -0300

    mediakit advances

--- src/pages/mediakit.tsx
+++ src/pages/mediakit.tsx
@@ -5,6 +5,7 @@ import Counters from 'components/sections/mediakit/counters'
 import MediakitHero from 'components/sections/mediakit/hero'
 import travel from '../../public/images/mediakit/travel.jpg'
 import art from '../../public/images/mediakit/art.jpg'
+import caba from '../../public/images/mediakit/caba.jpg'
 import Image from 'next/image'
 
 const Mediakit = () => {
@@ -23,7 +24,33 @@ const Mediakit = () => {
       >
         <MediakitHero />
       </div>
+      <FadeIn className="flex max-w-1440 justify-center items-center">
+        <Image src={caba} alt="caba" />
+      </FadeIn>
       <Container size="large" className="mt-3">
+        <div className="h-screen max-h-820 flex items-center justify-center">
+          <FadeIn>
+            <p className="text-18 opacity-90 max-w-xl text-center">
+              Innk ba es una revista digital de contenido original. Invita a los
+              escritores a compartir su palabra, a los viajeros a compartir sus
+              experiencias, a los pensadores a compartir sus ideas y a los
+              artistas a compartir sus obras.
+              <b>
+                Que lo que escribas no quede tirado en un cajón. Compartí lo
+                tuyo. Compartilo en Innk.
+              </b>
+            </p>
+          </FadeIn>
+        </div>
+        <div className="flex justify-between">
+          <FadeIn className="max-w-lg">
+            <Image src={art} alt="art" />
+          </FadeIn>
+          <FadeIn className="max-w-lg mt-52">
+            <Image src={travel} alt="travel" />
+          </FadeIn>
+        </div>
+        <Counters />
         <div className="flex items-center gap-20">
           <FadeIn>
             <video controls src={'/reel.mp4'} autoPlay muted width={400} />
@@ -44,26 +71,6 @@ const Mediakit = () => {
               </p>
             </FadeIn>
           </div>
-        </div>
-        <Counters />
-        <div className="flex justify-between">
-          <FadeIn className="max-w-lg">
-            <Image src={art} alt="art" />
-          </FadeIn>
-          <FadeIn className="max-w-lg mt-52">
-            <Image src={travel} alt="travel" />
-          </FadeIn>
-        </div>
-        <div className="h-screen max-h-820 flex items-center justify-center">
-          <FadeIn>
-            <p className="text-18 opacity-90 max-w-xl text-center">
-              Innk ba es una revista digital en la que cualquiera puede
-              escribir. ¿Los motivos? No existen. O sí, pueden ser todos. Los
-              que decidan formar parte de Innk no necesitan encontrar ese “algo”
-              que los justifique. Algunos escriben porque les gusta, otros para
-              darse a conocer y otros como un negocio, por ejemplo.
-            </p>
-          </FadeIn>
         </div>
       </Container>
     </PageLayout>
